9a8b6ba49a7e2289ce1356a7ed0f5bd6e97668fa
shubhambargal2002 <shubhambargal2002@gmail.com>
2023-02-09 10:44:20 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import connect from './database/conn.js';
+import router from './router/route.js';
+const port = 5000;
+
+const app = express();
+
+/** middlewares */
+app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'));
+app.disable('x-powered-by'); // less hackers know about our stack
+
+/** api routes */
+app.use('/api', router)
+
+/** start server only when we have valid connection */
+connect().then(() => {
+    try {
+        app.listen(port, () => {
+            console.log(`Auth-Server is listening at http://localhost:${port}`);
+        })
+    } catch (error) {
+        console.log('Cannot connect to the server')
+    }
+}).catch(error => {
+    console.log("Invalid database connection...!");
+})
+
